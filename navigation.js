50f81026838cfafcc5642ab3a9c805b5b211ce3c
noclue011 <94673977+noclue011@users.noreply.github.com>
2022-02-06 19:46:45 -0800

    Working on dark mode

--- navigation.js
+++ navigation.js
@@ -1,22 +1,43 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-
+// --- Sidebar Open/Close ---
+let sidebar = document.querySelector(".sidebar"),
+    closeBtn = document.querySelector("#menuBtn"),
+    searchBtn = document.querySelector(".bx-search");
+// Opens sidebar when sidebar icon is clicked
 closeBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("open");
-    menuBtnChange();//calling the function(optional)
+    menuBtnChange();
 });
-
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
+// Opens sidebar when search icon is clicked
+searchBtn.addEventListener("click", ()=>{
+    if (sidebar.classList.contains("open") == false) {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+    } else {
+        // This is where search functionality would be coded
+        console.log("search");
+    }
 });
-
-// following are the code to change sidebar button(optional)
+// Code to change the button for opening/closing the sidebar
 function menuBtnChange() {
     if(sidebar.classList.contains("open")){
         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-    }else {
+    } else {
         closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
     }
-}
+};
+// --- Dark Mode ---
+let body = document.querySelector('body'),
+    darkIcon = document.querySelector('#darkIcon'),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+// Toggle switch for dark mode
+modeSwitch.addEventListener("click" , () => {
+    body.classList.toggle("dark");
+    if(body.classList.contains("dark")) {
+        darkIcon.classList.replace("bx-moon", "bx-sun");
+        modeText.innerText = "Light mode";
+    } else {
+        darkIcon.classList.replace("bx-sun", "bx-moon");
+        modeText.innerText = "Dark mode";
+    }
+});
