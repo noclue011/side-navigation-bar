f5d8794cc03f946b379bcaefb64889d7e76773a7
noclue011 <94673977+noclue011@users.noreply.github.com>
2022-02-09 00:46:05 -0800

    Finishing sidebar template

--- navigation.js
+++ navigation.js
@@ -48,3 +48,13 @@ modeSwitch.addEventListener("click" , () => {
         modeText.innerText = "Dark mode";
     }
 });
+// --- Sub Menus ---
+let arrow = document.querySelectorAll(".arrow");
+
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
